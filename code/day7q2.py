2191137aa95bfec43753f07c30e71709ac730902
Arjun Mahajan <arjunmahajan@Arjun.local>
2021-12-07 09:22:07 -0500

    reverted back to floor

--- code/day7q2.py
+++ code/day7q2.py
@@ -1,25 +1,13 @@
 import math
 file = open("../inputs/day7.txt", "r")
 
-def getFinalPosition(positions, num):
-    toReturn = 0
-    for i in range(len(positions)):
-        if num > positions[i]:
-            if(len(positions)/2 > i):
-                toReturn = math.floor(num)
-            else:
-                toReturn = math.ceil(num)
-            break
-    return toReturn
-
 number_line = file.readline()
 horizontal_positions = number_line.split(",")
 horizontal_positions = [int(i) for i in horizontal_positions]
 horizontal_positions.sort()
 
 average = sum(horizontal_positions)/len(horizontal_positions)
-finalPosition = getFinalPosition(horizontal_positions, average)
-print(finalPosition)
+finalPosition = math.floor(average)
 fuel = 0
 
 def getFuel(num):
