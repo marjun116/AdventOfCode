a5f3dff1ce04ad62bc62aa8bc64f9e964b69fafc
Arjun Mahajan <arjunmahajan@Arjun.local>
2021-12-07 00:54:36 -0500

    made day 7 part 2 more generic

--- code/day7q2.py
+++ code/day7q2.py
@@ -1,12 +1,25 @@
 import math
 file = open("../inputs/day7.txt", "r")
 
+def getFinalPosition(positions, num):
+    toReturn = 0
+    for i in range(len(positions)):
+        if num > positions[i]:
+            if(len(positions)/2 > i):
+                toReturn = math.floor(num)
+            else:
+                toReturn = math.ceil(num)
+            break
+    return toReturn
+
 number_line = file.readline()
 horizontal_positions = number_line.split(",")
 horizontal_positions = [int(i) for i in horizontal_positions]
 horizontal_positions.sort()
-average = math.floor(sum(horizontal_positions)/len(horizontal_positions))
-print(average)
+
+average = sum(horizontal_positions)/len(horizontal_positions)
+finalPosition = getFinalPosition(horizontal_positions, average)
+print(finalPosition)
 fuel = 0
 
 def getFuel(num):
@@ -14,6 +27,6 @@ def getFuel(num):
 
 
 for position in horizontal_positions:
-    fuel+= getFuel(abs(average-position))
+    fuel+= getFuel(abs(finalPosition-position))
 
 print(fuel)
