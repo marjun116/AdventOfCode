e6b8ca0b7d93fbf8350ddee95b9f0f45fb9d2101
Arjun Mahajan <arjunmahajan@Arjun.local>
2021-12-12 18:10:20 -0600

    done with day 11

---
+++ code/day11q2.py
@@ -0,0 +1,58 @@
+with open("../inputs/day11.txt", 'r') as f:
+    data = dict()
+    for y, line in enumerate(f):
+        for x, val in enumerate(line.strip()):
+            data[x, y] = int(val)
+
+
+def getNeighbours(key):
+    x1, y1 = key
+    result = []
+    xvals = [-1, 0, +1]
+    yvals = [-1, 0, +1]
+    for x2 in xvals:
+        for y2 in yvals:
+            x = x1+x2
+            y = y1+y2
+            if x >= 0 and y >= 0 and x < 10 and y < 10:
+                result.append((x, y))
+    return result
+
+
+totalFlashes = 0
+
+
+def flash(flashed):
+    currFlashes = 0
+    global totalFlashes
+    flashing = []
+    for key in data:
+        if data[key] > 9 and key not in flashed:
+            flashing.append(key)
+            totalFlashes += 1
+            currFlashes += 1
+
+    if flashing:
+        for key in flashing:
+            neighbours = getNeighbours(key)
+            for neighbour in neighbours:
+                data[neighbour] += 1
+        return currFlashes + flash(flashed + flashing)
+    return currFlashes
+
+
+def increaseDay():
+    for key in data:
+        data[key] += 1
+    dayFlashes = flash([])
+    if dayFlashes == len(data):
+        print(day + 1)
+    for key in data:
+        if data[key] > 9:
+            data[key] = 0
+
+
+day = 0
+while day < 1000:
+    increaseDay()
+    day += 1
